dd5cd16d55ab554ad6cdc96976728e006ee36f32
jonathan YHWH <alphajorider@gmail.com>
2022-11-25 10:55:49 +0100

    search bar

--- index.php
+++ index.php
@@ -1,28 +1,12 @@
 <?php
-// function t(){
-//     $langue = parse_ini_file('fr.ini');
-    
-//         foreach ($langue as $l) {
-//             print $l;
-//             print "<br/>";
-//         };
-
-//     return $l;
-// }
-
 $langue = parse_ini_file('fr.ini');
-    
-foreach ($langue as $l) {
-    print $l;
-    print "<br/>";
-};
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
@@ -33,34 +17,19 @@ foreach ($langue as $l) {
     <?php
     include "./navbar.php";
     ?>
-    <article>
-
-    </article>
-    <article>
-        <form action="POST">
-            <button type="submit" form="original" value="original" name="original">
-                Version originale
-            </button>
-
-            <button type="submit" form="fr" value="francais">
-                Français
-            </button>
-
-
-            <button type="submit" form="en" value="anglais">
-                Anglais
-            </button>
-        </form>
-
-    </article>
-
-    <article>
+    <main>
         <?php
-        if (isset($_POST['original'])) {
-            echo t();
+        echo '<h3>' . 'Dictionaire français vers latin' . '</h3>';
+        foreach ($langue as $l => $v) {
+            echo $l . ' ' . '="' . $v;
+            echo "<br/>";
         };
+        echo '<form>';
+        echo '<input type="button" value="Go back!" onclick="history.back()">';
+        echo '</form>';
         ?>
-    </article>
+    </main>
+   
 </body>
 
 </html>
--- traduction.php
+++ traduction.php
@@ -1,91 +1,51 @@
-<?php
-function curl($url,$params = array(),$is_coockie_set = false)
-{
-if(!$is_coockie_set){
-/* STEP 1. let?s create a cookie file */
-$ckfile = tempnam ("/tmp", "CURLCOOKIE");
- /* STEP 2. visit the homepage to set the cookie properly */
-$ch = curl_init ($url);
-curl_setopt ($ch, CURLOPT_COOKIEJAR, $ckfile);
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-$output = curl_exec ($ch);
-}
-$str = ''; $str_arr= array();
-foreach($params as $key => $value)
-{
-$str_arr[] = urlencode($key)."=".urlencode($value);
-}
-if(!empty($str_arr))
-$str = '?'.implode('&',$str_arr);
+<!DOCTYPE html>
+<html lang="fr">
 
-/* STEP 3. visit cookiepage.php */
-
-$Url = $url.$str;
-$ch = curl_init ($Url);
-curl_setopt ($ch, CURLOPT_COOKIEFILE, $ckfile);
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-$output = curl_exec ($ch);
-return $output;
-}
-function Translate($word,$conversion = 'fr_to_es')
-{
-$word = urlencode($word);
-echo $word;
-echo "<br><br><br>";
-echo $conversion;
-$arr_langs = explode('_to_', $conversion);
-$url='http://translate.google.com/translate_a/t?client=t&text='.$word.'&hl="'.$arr_langs[0].'"&sl="'.$arr_langs[1].'"&tl="'.$arr_langs[0].'"&ie=UTF-8&oe=UTF-8&multires=1&otf=1&pc=1&trs=1&ssel=3&tsel=6&sc=1';
-echo $url;
-echo "<br><br><br>";
-$name_en = curl($url);
-$name_en = explode('"',$name_en);
-return  $name_en[1];
-}
- 
-?>
-<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>accueil</title>
 </head>
+
 <body>
-<form name="" method="post">
-<label>Traduction</label><br />
-<select name="langueorgine">
-<option value="fr" <?php if (isset($_POST['langueorgine']) && ($_POST['langueorgine']=='fr')){ echo "selected='selected'"; }?>>Francais</option> <!--Vous pouvez rajouter d'autres langues !-->
-<option value="en" <?php if (isset($_POST['langueorgine']) && ($_POST['langueorgine']=='en')){ echo "selected='selected'"; }?>>English</option>
-<option value="ar" <?php if (isset($_POST['langueorgine']) && ($_POST['langueorgine']=='ar')){ echo "selected='selected'"; }?>>Arabe</option>
-<option value="es" <?php if (isset($_POST['langueorgine']) && ($_POST['langueorgine']=='es')){ echo "selected='selected'"; }?>>Español</option>
-</select>
- 
-<select name="languetraduite">
-<option value="fr" <?php if (isset($_POST['languetraduite']) && ($_POST['languetraduite']=='fr')){ echo "selected='selected'"; }?>>Francais</option> <!--Vous pouvez rajouter d'autres langues !-->
-<option value="en" <?php if (isset($_POST['languetraduite']) && ($_POST['languetraduite']=='en')){ echo "selected='selected'"; }?>>English</option>
-<option value="ar" <?php if (isset($_POST['languetraduite']) && ($_POST['languetraduite']=='ar')){ echo "selected='selected'"; }?>>Arabe</option>
-<option value="es" <?php if (isset($_POST['languetraduite']) && ($_POST['languetraduite']=='es')){ echo "selected='selected'"; }?>>Español</option>
-</select>
-			<textarea name="traduire"  rows="8"><?php if (isset($_POST['traduire'])) echo $_POST['traduire'] ; ?></textarea><br />
-			<input class="btn btn-primary" name="ok" type="submit"   value="Traduire" />
-</form>
- 
-<?php
-		 $chaine="";
-	if (isset($_POST['traduire'])){
-                   $chaine=$_POST['traduire'];
-      	 }
-		 $langueorgine="fr";
-	if (isset($_POST['langueorgine'])){
-                   $langueorgine=$_POST['langueorgine'];
-				   echo "Langue orgine :".$langueorgine;
-      	 }
-		 $languetraduite="en";
-	if (isset($_POST['languetraduite'])){
-                   $languetraduite=$_POST['languetraduite'];
-				   echo "<br> Langue traduite :".$languetraduite;
-      	 }
-/* STEP 4. IMPLEMENTATION */
-echo "<br><br>";
-echo  Translate($chaine, $langueorgine.'_to_'.$languetraduite);
-echo "<br><br><br>";
-?>
+	<?php
+	include "./navbar.php";
+	?>
+	<main>
+		<?php
+		$mot = $_GET['therme'];
+		function t($mot)
+		{
+			$dictionnaire = parse_ini_file('fr.ini');
+
+			foreach ($dictionnaire as $l => $v) {
+				$fr = array($l);
+				$lt = array($v);
+			};
+
+			return $fr;
+		}
+
+		if (!empty($mot)) {
+			// echo '<h3>' . 'vous avez recherché le therme: '.$v.'</h3>';
+			// echo '<a href="/">' . 'réinitialiser' . "</a>";  
+			if (t($mot)) {
+				echo '<li>' . $langue[$mot] . "</li>";
+				echo '<form>';
+				echo '<input type="button" value="Go back!" onclick="history.back()">';
+				echo '</form>';
+			} else {
+				echo '<h3>' . 'vous avez recherché le therme: ' . $mot . '</h3>';
+				echo '<form>';
+				echo '<input type="button" value="Go back!" onclick="history.back()">';
+				echo '</form>';
+			}
+		}
+		?>
+	</main>
+
 </body>
+
 </html>
